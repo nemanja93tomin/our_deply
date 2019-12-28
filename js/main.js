9ceb6400b128f7da642f894b30d5730fe82c1165
Nemanja Tomin <nemanja93tomin@gmail.com>
2019-12-28 20:40:40 +0100

    added scroll effect on menu

--- js/main.js
+++ js/main.js
@@ -9,31 +9,35 @@
      * events
      */
 
-    let headerContainer = $('.head-content-wrapper');
-    let footerContainer = $('.footer-content-wrapper');
-
-    // footerContainer.load('footer.html');
-
     let navigationLink = $('.navigation').find('a');
     let footerLink  = $('.footer-menu').find('a');
-    // console.log(navigationLink);
-    // console.log(footerLink);
-    let active = 0;
 
-    navigationLink.click(function(ev) {
+    // Main manu with scroll effect
+    navigationLink.click(function() {
+        // Take data-target atribut from the clicked link
         let target = $(this).attr('data-target');
+        // Variable index contain index of parent of the clicked link
         let index = $(this).parent().index();
-        let offset = Math.abs(index - active);
-        // console.log(offset);
+        // Animate scroll effect for a period of 500ms multiplied with current index plus 1
         $('html, body').animate({
-            'scroll-top': $(target).offset().top
-        }, 500 * offset
-        );
-        active = index;
+            scrollTop: $(target).offset().top
+        },500 * (index+1));
     });
 
+    //Same scroll effect on footer menu
+    footerLink.click(function() {
+        let target = $(this).attr('data-target');
+        let index = $(this).parent().index();
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        },500 * (index+1));
+    });
+
+    //Create on method for submit button in contact form
     $('#contact-form').on('submit', function(event) {
+        //the default action of the event will not be triggered. In this case will not refresh page, before the function is executed
         event.preventDefault();
+        //Create object with the same fields as in form, take thair value and places in appropriate properties
         let user = {
             name    : $('#name').val(),
             lastName: $('#lastname').val(),
